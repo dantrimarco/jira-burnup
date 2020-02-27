9e472a6be562254932dbe74a2982850da0b9ace5
Dan Trimarco <dan.trimarco@DTRIMARCO-MB.vividseats.local>
2020-02-27 11:45:00 -0600

    config and app.py changes

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ issues_df = jb.get_issues(jira, query)
 
 completed_points_per_sprint = jb.aggregate_completed_points(issues_df)
 
-sprint_df = jb.get_sprint_list(jira, config['jira_project_id'], sprint_id_start=0, sprint_id_end=9999)
+sprint_df = jb.get_sprint_list(jira, config['jira_board_id'], sprint_id_start=0, sprint_id_end=9999)
 
 sprint_data = jb.create_total_scope_data(issues_df, completed_points_per_sprint, sprint_df, export=False, sprint_data_filename='sprint_data.csv')
 
